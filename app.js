b1db9008093287444b787bccd8b3edce6a4897ff
oana-iu <oana.mitri01@gmail.com>
2024-06-10 11:06:06 +0300

    add product and review models

--- app.js
+++ app.js
@@ -13,19 +13,31 @@ const connectDB = require("./db/connect");
 
 //router
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
+    console.log(req.cookies);
     res.send("E-commerce-API");
 });
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const jwt = require("jsonwebtoken");
+const { attachCookiesToResponse, createTokenUser } = require("../utils");
 
 const register = async (req, res) => {
     const { email, name, password } = req.body;
@@ -17,22 +17,45 @@ const register = async (req, res) => {
 
     const user = await User.create({ email, name, password, role });
 
-    console.log(process.env.JWT_LIFETIME);
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
 
-    const tokenUser = { name: user.name, userId: user._id, role: user.role };
-    const token = jwt.sign(tokenUser, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_LIFETIME,
-    });
-
-    res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
+    res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
-    res.send("Login user");
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new CustomError.BadRequestError(
+            "Please provide email and password "
+        );
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new CustomError.UnauthenticatedError("Invalid credentials ");
+    }
+
+    const isPasswordCorrect = user.comparePassword(password);
+
+    if (!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError("Invalid credentials ");
+    }
+
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
+
+    res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
-    res.send("logout user");
+    res.cookie("token", "logout", {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    });
+
+    res.status(StatusCodes.OK).json({ msg: "User was logged out" });
 };
 
 module.exports = { register, login, logout };
